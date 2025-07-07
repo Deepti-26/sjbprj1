85e746c5f12358aa3fd5010b2eed7bfb67fd8978
shobhadevi <Deepti-26>
2025-07-07 16:03:19 +0530

    my first commit

---
+++ src/dsaprj/mca/dsa/Student.java
@@ -0,0 +1,12 @@
+package dsaprj.mca.dsa;
+
+import java.util.concurrent.StructuredTaskScope.ShutdownOnSuccess;
+
+public class Student {
+
+	public static void main(String[] args) {
+        System.out.println("hi");
+
+	}
+
+}
