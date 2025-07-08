c1e21407f8d2951ecc83622c9893fda72c6362aa
shobhadevi <Deepti-26>
2025-07-08 11:01:27 +0530

    updated code

---
+++ src/dsaprj/mca/dsa/Demo.java
@@ -0,0 +1,13 @@
+package dsaprj.mca.dsa;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int c;
+		c=a+b;
+	    System.out.println("sum of"+c);
+
+	}
+
+}
---
+++ src/dsaprj/mca/dsa/Person.java
@@ -0,0 +1,8 @@
+package dsaprj.mca.dsa;
+
+public class Person {
+	void f1() {
+		System.out.println("abc");
+	}
+
+}
